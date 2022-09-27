d9ce0bc40be4f9a2ddecb94b94069bf9894ce276
muchirbickel <muchir99@gmail.com>
2022-09-26 20:46:40 -0400

    complete readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,87 +1,80 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function RandomInt(min, max) {
-  if (!max) {
-    max = min
-    min = 0
-  }
-  var rand = Math.random()
-  return Math.floor(min * (1 - rand) + rand * max)
-
-  function getRandomItem(list) {
-    return list[randomInt(list.length)]
-  }
-
-  function generaPassword() {
-
-    while (true) {
-      var userInput = window.prompt("how long you set your password?")
-      var passwordlength = parseInt(userInput)
-
-  if (isNaN(passwordlength)) {
-        window.alert("That's not a number!")
-      } else if (passwordlength < 8 || passwordlength > 128) {
-        window.alert("Invalid password length.length should be between 8-128 characters.")
-      } else {
-        break
-      }
-    }
-    
-    var userwantsNumbers = window.confirm("would you like to include numbers in your password? ")
-    var userwantsSymbols = window.confirm("would you like to include symbols in your password? ")
-    var userwantsLowercase = window.confirm("would you like to include Lowercase in your password? ")
-    var userwantsUppercase = window.confirm("would you like to include Uppercase in your password? ")
-
-
-    var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"]
-    var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    var uppercaseList = []
-
-    for (var i = 0; i < lowercaseList.length; i++) {
-      uppercaseList[i] = lowercaseList[i].toUpperCase()
-    }
-
-    if (userwantsNumbers === true) {
-      optionsCart.push(numberList)
-    }
-
-    if (userwantsSymbols === true) {
-      optionsCart.push(SymbolList)
-    }
-
-    if (userwantsLowercase === true) {
-      optionsCart.push(LowercaseList)
-    }
-
-    if (userwantsUppercase === true) {
-      optionsCart.push(UppercaseList)
-    }
-
-    if (optioncart.length === 0) {
-      optionCart.push(lowercaseList)
-    }
-
-    var generatedPassword = ""
-
-    for (var i = 0; i < passwordlength; i++) {
-      var randomlist = getRandomItem(optionsCart)
-      var randomchar = getRandomItem(randomlist)
-      console.log(randomchar)
-    }
-  }
-
-  return generaPassword
-}
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+// generate password setting
+function generatePassword() {
+  var originalLength = passwordLength();
+  var upperLower = getUpperLower();
+  var specialCharacter = getSpecialCharacter();
+
+  var length = originalLength - upperLower.length;
+  length = length - specialCharacter.length;
+  var randomNum = getRandomNum(length);
+
+  var password = upperLower + specialCharacter;
+  for (rand of randomNum) {
+    password += rand;
+  }
+  return password.slice(0, originalLength);
+}
+
+// get password length
+function passwordLength(){
+  var passLength;
+  var bool = true;
+  do {
+     passLength = window.prompt("Please choose password length between 8 and 128");
+     if(passLength >= 8 || passLength <= 128){
+      bool = false;
+     }
+  } while (bool);
+  return passLength;
+}
+
+// set uppercase and lowercase character
+function getUpperLower() {
+  var caseLetter;
+  do {
+    var bool = false;
+    caseLetter = window.prompt("Please enter the uppercase or lowercase character");
+    if (!isNaN(caseLetter)) {
+      bool = true;
+    }
+  } while (bool);
+  return caseLetter;
+}
+
+// set special character
+function getSpecialCharacter() {
+  var specialChar;
+  var characterArray = [ "!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","_","`","|","~" ] ;
+  do {
+    var bool = true;
+    specialChar = window.prompt("Please choose special character from {!,#,$,%,&,(,),*,+,-,.,/,:,;,<,=,>,?,@,^,_,`,|,~}");
+    for (var i = 0; i < characterArray.length; i++) {
+      if (specialChar == characterArray[i]) {
+        bool = false;
+      }
+    }
+  } while (bool);
+  return specialChar;
+}
+
+// set to generate random number
+function getRandomNum(max) {
+  var randNum = [max];
+  for (var i = 0; i < max; i++) {
+    randNum[i] = (Math.floor(Math.random() * max));
+  }
+  return randNum;
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,22 +1,14 @@
 # Password Generator
 
-## Your Task
-
-This week's Challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
-
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
 
 ## User Story
 
-```
-AS AN employee with access to sensitive data
+When AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+SO THAT I can create a strong password that provides greater security.
 
 ## Acceptance Criteria
 
-```
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria
@@ -32,11 +24,11 @@ WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
-```
 
-## Mock-Up
+## screenshot
 
 The following image shows the web application's appearance and functionality:
+
 
 ![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
 
