3bf43a826d4dac1a009392a2d5c5e4ffb87f7003
muchirbickel <muchir99@gmail.com>
2022-10-13 23:30:43 -0400

    update assignment

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// generate password setting
-function generatePassword() {
+// function to generate password
+function generatePassword(){
   var originalLength = passwordLength();
   var upperLower = getUpperLower();
   var specialCharacter = getSpecialCharacter();
@@ -20,13 +20,13 @@ function generatePassword() {
   var randomNum = getRandomNum(length);
 
   var password = upperLower + specialCharacter;
-  for (rand of randomNum) {
+  for (rand of randomNum){
     password += rand;
   }
   return password.slice(0, originalLength);
 }
 
-// get password length
+// fucntion to get password length
 function passwordLength(){
   var passLength;
   var bool = true;
@@ -39,28 +39,28 @@ function passwordLength(){
   return passLength;
 }
 
-// set uppercase and lowercase character
-function getUpperLower() {
-  var caseLetter;
-  do {
+// function to get uppercase or lowercase character
+function getUpperLower(){
+  var caseLetter; 
+  do{
     var bool = false;
     caseLetter = window.prompt("Please enter the uppercase or lowercase character");
-    if (!isNaN(caseLetter)) {
+    if(!isNaN(caseLetter)){
       bool = true;
     }
   } while (bool);
   return caseLetter;
 }
 
-// set special character
-function getSpecialCharacter() {
+// function to get special character
+function getSpecialCharacter(){
   var specialChar;
-  var characterArray = [ "!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","_","`","|","~" ] ;
-  do {
+  var characterArray = ["!", "#", "$", "%", "&", "'", "*", "-", "?", "@", "_"];
+  do{
     var bool = true;
-    specialChar = window.prompt("Please choose special character from {!,#,$,%,&,(,),*,+,-,.,/,:,;,<,=,>,?,@,^,_,`,|,~}");
-    for (var i = 0; i < characterArray.length; i++) {
-      if (specialChar == characterArray[i]) {
+    specialChar = window.prompt("Please choose cpecial character from {!, #, $, %, &, ', *, -, ?, @, _ }");
+    for (var i = 0; i < characterArray.length; i++){
+      if(specialChar == characterArray[i]){
         bool = false;
       }
     }
@@ -68,14 +68,16 @@ function getSpecialCharacter() {
   return specialChar;
 }
 
-// set to generate random number
-function getRandomNum(max) {
+// function to generate random number
+function getRandomNum(max){
   var randNum = [max];
-  for (var i = 0; i < max; i++) {
-    randNum[i] = (Math.floor(Math.random() * max));
+  for (var i = 0; i < max; i++){
+    randNum[i] =(Math.floor(Math.random() * max));
   }
   return randNum;
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
